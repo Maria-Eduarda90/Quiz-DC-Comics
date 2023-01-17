102c57c527f8836ce87e76b225881f276f1e0951
Mary Skywalker <snowdennaruto90@gmail.com>
2023-01-17 13:53:53 -0400

    added questions

---
+++ src/data/questions.ts
@@ -0,0 +1,122 @@
+export const data = [
+  {
+    question: "Quem foi o quadrinista responsável pela criação da Liga da Justiça?",
+    options: [
+        "Will Jacobs", 
+        "Robert Kanigher", 
+        "Gardner Fox", 
+        "Joe Kubert"
+    ],
+    answer: "Gardner Fox",
+  },
+  {
+    question: "Qual herói não estava na primeira formação da Liga da Justiça?",
+    options: [
+      "Caçador de Marte",
+      "Arqueiro Verde",
+      "Lanterna Verde",
+      "Flash",
+    ],
+    answer: "Arqueiro Verde",
+  },
+  {
+    question: "Qual é o nome completo do primeiro Robin?",
+    options: [
+      "Richard John Grayson",
+      "Richard Jon Grayson",
+      "Richard Jhon Grayson",
+      "Richard Dick Grayson",
+    ],
+    answer: "Richard John Grayson",
+  },
+  {
+    question: "Com o lançamento dos Novos 52, a origem da Liga da Justiça foi alterada e um outro herói substituiu o Caçador de Marte como um dos membros fundadores da equipe. De quem estamos falando?",
+    options: [
+      "Ciborgue",
+      "Gavião Negro",
+      "Eléktron",
+      "Mulher-Gavião",
+    ],
+    answer: "Ciborgue",
+  },
+  {
+    question: "Qual das bases secretas da Liga da Justiça também foi usada pela Justiça Jovem?",
+    options: [
+      "O Santuário Secreto",
+      "O Satélite",
+      "O Bunker",
+      "A Torre de Vigilância",
+    ],
+    answer: "O Santuário Secreto",
+  },
+  {
+    question: "Qual das bases secretas da Liga da Justiça também foi usada pela Justiça Jovem?",
+    options: [
+      "O Santuário Secreto",
+      "O Satélite",
+      "O Bunker",
+      "A Torre de Vigilância",
+    ],
+    answer: "O Santuário Secreto",
+  },
+  {
+    question: "Qual personagem clássico da equipe nos quadrinhos não faz parte da formação oficial dos heróis no desenho animado da Liga da Justiça??",
+    options: [
+      "Caçador de Marte",
+      "Lanterna Verde",
+      "Aquaman",
+      "Mulher-Gavião",
+    ],
+    answer: "Aquaman",
+  },
+  {
+    question: "Em que ano foi lançada a primeira adaptação em live-action da Liga da Justiça?",
+    options: [
+      "1997",
+      "2007",
+      "2016",
+      "2017",
+    ],
+    answer: "1997",
+  },
+  {
+    question: 'De acordo com a DC Comics, qual destes personagens é considerado "o maior super-herói do mundo"?',
+    options: [
+      "Superman",
+      "Batman",
+      "Flash",
+      "Lanterna verde",
+    ],
+    answer: "Superman",
+  },
+  {
+    question: "Qual destas foi a primeira animação lançada pela DC Comics/Warner?",
+    options: [
+      "Batman do Futuro: O Retorno do Coringa",
+      "Batman vs. Drácula",
+      "Superman: Brainiac Contra-Ataca",
+      "Batman: A Máscara do Fantasma",
+    ],
+    answer: "Batman: A Máscara do Fantasma",
+  },
+  {
+    question: "Qual é a ordem dos Robins?",
+    options: [
+      "Dick Grayson, Jason Todd, Damian Wayne, Stephanie Brown e Tim Drake",
+      "Dick Grayson, Tim Drake, Jason Todd, Stephanie Brown e Damian Wayne",
+      "Dick Grayson, Jason Todd, Tim Drake, Stephanie Brown e Damian Wayne",
+      "Dick Grayson, Stephanie Brown, Jason Todd, Damian e Tim Drake",
+    ],
+    answer: "Dick Grayson, Jason Todd, Tim Drake, Stephanie Brown e Damian Wayne",
+  },
+  {
+    question: "Em que HQ o jason morre?",
+    options: [
+      "Batman Ego",
+      "Batman a morte",
+      "Morte em familia",
+      "Batman Vitoria Sombria",
+    ],
+    answer: "Morte em familia",
+  },
+];
