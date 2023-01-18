41f2e725ba58d8804222480b8c1e1198b88e9bcb
Mary Skywalker <snowdennaruto90@gmail.com>
2023-01-17 23:25:45 -0400

    starting reducer

--- src/context/QuizContext.tsx
+++ src/context/QuizContext.tsx
@@ -1,14 +1,36 @@
-import { createContext, ReactNode } from 'react';
+import { createContext, ReactNode, useReducer } from 'react';
+import { question } from '../data/questions';
+
+type Action = | { type: 'CHANGE_STATE'}
 
 type ElementChildren = {
     children: ReactNode;
+};
+
+const STAGES = ["Start", "Playing", "End"];
+
+const initialState = {
+    gameStage: STAGES[0],
+    question,
+};
+
+function quizReducer(state: any, action: Action){
+    console.log(state, action);
+    switch(action.type){
+        case "CHANGE_STATE":
+            return state;
+        default:
+            return state;
+    }
 }
 
-export const QuizContext = createContext({});
+export const QuizContext = createContext({} as any);
 
 export function QuizProvider({ children }: ElementChildren){
+    const value = useReducer(quizReducer, initialState);
+
     return(
-        <QuizContext.Provider value={{ name: 'maria' }}>
+        <QuizContext.Provider value={value}>
             {children}
         </QuizContext.Provider>
     );
