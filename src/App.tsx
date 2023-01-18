4fc736165f2ce7d580977675c0cdccf41bf813a3
Mary Skywalker <snowdennaruto90@gmail.com>
2023-01-18 00:06:45 -0400

    feat: checking what state the player is in, if it is in start show the welcome component, if not show the questions component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Questions } from "./components/Questions";
 import { Welcome } from "./components/Welcome";
+import { useQuiz } from "./hook/useQuiz";
 
 export function App() {
+  const [quizState, dispatch] = useQuiz();
+  
   return (
-    <Welcome/>
+    <>
+      {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Playing" && <Questions />}
+    </>
   )
 }
