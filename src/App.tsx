49ebcf465141d088f0a2567ef8b71b2e9f633d54
Mary Skywalker <snowdennaruto90@gmail.com>
2023-01-18 22:17:27 -0400

    feat: added gameover component for when you finish the quiz

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
+import { useEffect } from "react";
+import { GameOver } from "./components/GameOver";
 import { Questions } from "./components/Questions";
 import { Welcome } from "./components/Welcome";
 import { useQuiz } from "./hook/useQuiz";
 
 export function App() {
   const [quizState, dispatch] = useQuiz();
+
+  useEffect(() => {
+    dispatch({ type: "REORDER_QUESTIONS" });
+  }, []);
   
   return (
     <>
       {quizState.gameStage === "Start" && <Welcome />}
       {quizState.gameStage === "Playing" && <Questions />}
+      {quizState.gameStage === "End" && <GameOver/>}
     </>
   )
 }
