d6dbf8346d7d07c0a73b13582f8b2930c48b4923
Mary Skywalker <snowdennaruto90@gmail.com>
2023-01-18 22:22:08 -0400

    feat: logic for when the questions are over, show the final component

--- src/context/QuizContext.tsx
+++ src/context/QuizContext.tsx
@@ -1,7 +1,7 @@
 import { createContext, ReactNode, useReducer } from 'react';
 import { question } from '../data/questions';
 
-type Action = | { type: 'CHANGE_STATE'};
+type Action = | { type: 'CHANGE_STATE' } | { type: 'REORDER_QUESTIONS' } | { type: 'CHANGE_QUESTION' };
 
 type ElementChildren = {
     children: ReactNode;
@@ -12,6 +12,7 @@ const STAGES = ["Start", "Playing", "End"];
 const initialState = {
     gameStage: STAGES[0],
     question,
+    currentQuestion: 0,
 };
 
 function quizReducer(state: any, action: Action){
@@ -22,6 +23,30 @@ function quizReducer(state: any, action: Action){
                 ...state,
                 gameStage: STAGES[1],
             };
+        case "REORDER_QUESTIONS":
+            const reorderedQuestions = question.sort(() => {
+                return Math.random() - 0.5;
+            });
+
+            return {
+                ...state,
+                question: reorderedQuestions,
+            };
+
+        case "CHANGE_QUESTION":
+            const nextQuestion = state.currentQuestion + 1;
+            let endGame = false;
+
+            if(!question[nextQuestion]){
+                endGame = true;
+            }
+
+            return {
+                ...state,
+                currentQuestion: nextQuestion,
+                gameStage: endGame ? STAGES[2] : state.gameStage,
+            };
+
         default:
             return state;
     }
