0a122e6ffece696646ff66bf52f2855372673e07
Mary Skywalker <snowdennaruto90@gmail.com>
2023-01-18 22:15:09 -0400

    feat: dispatch to change component when questions run out

--- src/components/Questions/index.tsx
+++ src/components/Questions/index.tsx
@@ -1,7 +1,24 @@
+import { useQuiz } from '../../hook/useQuiz';
 import styles from './styles.module.scss';
 
 export function Questions(){
+    const [quizState, dispatch] = useQuiz();
+    const currentQuestion = quizState.question[quizState.currentQuestion];
+    
     return(
-        <h1>Game</h1>
+        <div className={styles.container}>
+            <div className={styles.questions}>
+                <p>
+                    Pergunta {quizState.currentQuestion + 1} de {quizState.question.length}
+                </p>
+                <h2>
+                    {currentQuestion.question}
+                </h2>
+                <div className={styles.optionsContainer}>
+                    <p>Opções</p>
+                </div>
+                <button onClick={() => dispatch({ type: "CHANGE_QUESTION" })}>Continuar</button>
+            </div>
+        </div>
     );
 }
